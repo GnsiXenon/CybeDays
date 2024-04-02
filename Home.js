144d741ec03d19b35227a9c125b21dda402512bc
GnsiXenon <kylianoger@orange.fr>
2024-04-02 18:41:53 +0200

    push

--- Home.js
+++ Home.js
@@ -3,17 +3,21 @@ var i = 0;
 function move() {
   if (i == 0) {
     i = 1;
+    var a = document.getElementById("a");
     var elem = document.getElementById("myBar");
     var info = document.getElementById("text");
-    var image = document.getElementById("image");
+    var image = document.getElementById("b");
     var width = 0;
+    image.style.display = "none";
     var id = setInterval(frame, 100)
     function frame() {
       if (width >= 100) {
         clearInterval(id);
         i = 0;
         info.innerHTML = "Upload Complete";
-        image.style.display = "block";
+        image.style.display = "flex";
+        a.style.display = "none";
+
       } else {
         width++;
         elem.style.width = width + "%";
